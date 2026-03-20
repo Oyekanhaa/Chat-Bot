d318855d968bca268766204ecb00c83f937d12f5
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2026-03-20 11:53:06 +0530

    Add files via upload

---
+++ MeowChat/__init__.py
@@ -0,0 +1,9 @@
+import os
+from pyrogram import Client
+
+app = Client(
+    "session",  # This is the session name
+    api_id=int(os.environ.get("API_ID")),
+    api_hash=os.environ.get("API_HASH"),
+    session_string=os.environ.get("STRING_SESSION")
+)
